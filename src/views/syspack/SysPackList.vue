1d18927d82974b482f68e511bad6bb81b69178a2
zhouyl <Administrator@DESKTOP-8BI2EMU>
2024-09-23 22:14:51 +0800

    重新生产pack

--- src/views/syspack/SysPackList.vue
+++ src/views/syspack/SysPackList.vue
@@ -1,93 +1,191 @@
 <template>
-  <div>
+  <div class="p-2">
+    <!--查询区域-->
+    <div class="jeecg-basic-table-form-container">
+      <a-form ref="formRef" @keyup.enter.native="searchQuery" :model="queryParam" :label-col="labelCol" :wrapper-col="wrapperCol">
+        <a-row :gutter="24">
+          <a-col :lg="6">
+            <a-form-item name="packName">
+              <template #label><span title="产品名称">产品名称</span></template>
+              <a-input placeholder="请输入产品名称" v-model:value="queryParam.packName" allow-clear ></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :lg="6">
+            <a-form-item name="packCode">
+              <template #label><span title="产品编码">产品编码</span></template>
+              <a-input placeholder="请输入产品编码" v-model:value="queryParam.packCode" allow-clear ></a-input>
+            </a-form-item>
+          </a-col>
+          <template v-if="toggleSearchStatus">
+            <a-col :lg="6">
+              <a-form-item name="category">
+                <template #label><span title="产品类别(单机版/云端版)">产品类别</span></template>
+                <a-input placeholder="请输入产品类别(单机版/云端版)" v-model:value="queryParam.category" allow-clear ></a-input>
+              </a-form-item>
+            </a-col>
+            <a-col :lg="6">
+              <a-form-item name="type">
+                <template #label><span title="产品类型(送货单版/进销存版)">产品类型</span></template>
+                <a-input placeholder="请输入产品类型(送货单版/进销存版)" v-model:value="queryParam.type" allow-clear ></a-input>
+              </a-form-item>
+            </a-col>
+            <a-col :lg="6">
+              <a-form-item name="orgNum">
+                <template #label><span title="支持企业数(单机1个公司、云端版支持4家公司切换开单)">支持企业</span></template>
+                <a-input-number placeholder="请输入支持企业数(单机1个公司、云端版支持4家公司切换开单)" v-model:value="queryParam.orgNum"></a-input-number>           
+              </a-form-item>
+            </a-col>
+            <a-col :lg="6">
+              <a-form-item name="accountNum">
+                <template #label><span title="支持账号数(云端版支持添加2个子账号，授权后最大支持添加12个子账号)">支持账号</span></template>
+                <a-input-number placeholder="请输入支持账号数(云端版支持添加2个子账号，授权后最大支持添加12个子账号)" v-model:value="queryParam.accountNum"></a-input-number>           
+              </a-form-item>
+            </a-col>
+            <a-col :lg="6">
+              <a-form-item name="goodsNum">
+                <template #label><span title="支持商品数量()">支持商品</span></template>
+                <a-input-number placeholder="请输入支持商品数量()" v-model:value="queryParam.goodsNum"></a-input-number>           
+              </a-form-item>
+            </a-col>
+            <a-col :lg="6">
+              <a-form-item name="price">
+                <template #label><span title="产品标准价格">产品标准</span></template>
+                <a-input-number placeholder="请输入产品标准价格" v-model:value="queryParam.price"></a-input-number>           
+              </a-form-item>
+            </a-col>
+            <a-col :lg="6">
+              <a-form-item name="discounted">
+                <template #label><span title="产品折扣">产品折扣</span></template>
+                <a-input-number placeholder="请输入产品折扣" v-model:value="queryParam.discounted"></a-input-number>           
+              </a-form-item>
+            </a-col>
+            <a-col :lg="6">
+              <a-form-item name="discountedPrice">
+                <template #label><span title="产品折扣价">产品折扣</span></template>
+                <a-input-number placeholder="请输入产品折扣价" v-model:value="queryParam.discountedPrice"></a-input-number>           
+              </a-form-item>
+            </a-col>
+            <a-col :lg="6">
+              <a-form-item name="specification">
+                <template #label><span title="规格">规格</span></template>
+                <a-input-number placeholder="请输入规格" v-model:value="queryParam.specification"></a-input-number>           
+              </a-form-item>
+            </a-col>
+            <a-col :lg="6">
+              <a-form-item name="specificationUnit">
+                <template #label><span title="规格单位">规格单位</span></template>
+                <a-input placeholder="请输入规格单位" v-model:value="queryParam.specificationUnit" allow-clear ></a-input>
+              </a-form-item>
+            </a-col>
+            <a-col :lg="6">
+              <a-form-item name="status">
+                <template #label><span title="产品启用状态(1 启用, 0 停用)">产品启用</span></template>
+                <a-input placeholder="请输入产品启用状态(1 启用, 0 停用)" v-model:value="queryParam.status" allow-clear ></a-input>
+              </a-form-item>
+            </a-col>
+          </template>
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <span style="float: left; overflow: hidden" class="table-page-search-submitButtons">
+              <a-col :lg="6">
+                <a-button type="primary" preIcon="ant-design:search-outlined" @click="searchQuery">查询</a-button>
+                <a-button type="primary" preIcon="ant-design:reload-outlined" @click="searchReset" style="margin-left: 8px">重置</a-button>
+                <a @click="toggleSearchStatus = !toggleSearchStatus" style="margin-left: 8px">
+                  {{ toggleSearchStatus ? '收起' : '展开' }}
+                  <Icon :icon="toggleSearchStatus ? 'ant-design:up-outlined' : 'ant-design:down-outlined'" />
+                </a>
+              </a-col>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
     <!--引用表格-->
-   <BasicTable @register="registerTable" :rowSelection="rowSelection">
-     <!--插槽:table标题-->
+    <BasicTable @register="registerTable" :rowSelection="rowSelection">
+      <!--插槽:table标题-->
       <template #tableTitle>
-          <a-button type="primary" v-auth="'syspack:sys_pack:add'" @click="handleAdd" preIcon="ant-design:plus-outlined"> 新增</a-button>
-          <a-button  type="primary" v-auth="'syspack:sys_pack:exportXls'" preIcon="ant-design:export-outlined" @click="onExportXls"> 导出</a-button>
-          <j-upload-button type="primary" v-auth="'syspack:sys_pack:importExcel'" preIcon="ant-design:import-outlined" @click="onImportXls">导入</j-upload-button>
-          <a-dropdown v-if="selectedRowKeys.length > 0">
-              <template #overlay>
-                <a-menu>
-                  <a-menu-item key="1" @click="batchHandleDelete">
-                    <Icon icon="ant-design:delete-outlined"></Icon>
-                    删除
-                  </a-menu-item>
-                </a-menu>
-              </template>
-              <a-button v-auth="'syspack:sys_pack:deleteBatch'">批量操作
-                <Icon icon="mdi:chevron-down"></Icon>
-              </a-button>
+        <a-button type="primary" v-auth="'syspack:sys_pack:add'"  @click="handleAdd" preIcon="ant-design:plus-outlined"> 新增</a-button>
+        <a-button  type="primary" v-auth="'syspack:sys_pack:exportXls'" preIcon="ant-design:export-outlined" @click="onExportXls"> 导出</a-button>
+        <j-upload-button  type="primary" v-auth="'syspack:sys_pack:importExcel'"  preIcon="ant-design:import-outlined" @click="onImportXls">导入</j-upload-button>
+        <a-dropdown v-if="selectedRowKeys.length > 0">
+          <template #overlay>
+            <a-menu>
+              <a-menu-item key="1" @click="batchHandleDelete">
+                <Icon icon="ant-design:delete-outlined"></Icon>
+                删除
+              </a-menu-item>
+            </a-menu>
+          </template>
+          <a-button v-auth="'syspack:sys_pack:deleteBatch'">批量操作
+            <Icon icon="mdi:chevron-down"></Icon>
+          </a-button>
         </a-dropdown>
         <!-- 高级查询 -->
         <super-query :config="superQueryConfig" @search="handleSuperQuery" />
       </template>
-       <!--操作栏-->
+      <!--操作栏-->
       <template #action="{ record }">
         <TableAction :actions="getTableAction(record)" :dropDownActions="getDropDownAction(record)"/>
       </template>
-      <!--字段回显插槽-->
       <template v-slot:bodyCell="{ column, record, index, text }">
       </template>
     </BasicTable>
     <!-- 表单区域 -->
-    <SysPackModal @register="registerModal" @success="handleSuccess"></SysPackModal>
+    <SysPackModal ref="registerModal" @success="handleSuccess"></SysPackModal>
   </div>
 </template>
 
 <script lang="ts" name="syspack-sysPack" setup>
-  import {ref, reactive, computed, unref} from 'vue';
-  import {BasicTable, useTable, TableAction} from '/@/components/Table';
-  import {useModal} from '/@/components/Modal';
-  import { useListPage } from '/@/hooks/system/useListPage'
-  import SysPackModal from './components/SysPackModal.vue'
-  import {columns, searchFormSchema, superQuerySchema} from './SysPack.data';
-  import {list, deleteOne, batchDelete, getImportUrl,getExportUrl} from './SysPack.api';
+  import { ref, reactive } from 'vue';
+  import { BasicTable, useTable, TableAction } from '/@/components/Table';
+  import { useListPage } from '/@/hooks/system/useListPage';
+  import { columns, superQuerySchema } from './SysPack.data';
+  import { list, deleteOne, batchDelete, getImportUrl, getExportUrl } from './SysPack.api';
   import { downloadFile } from '/@/utils/common/renderUtils';
+  import SysPackModal from './components/SysPackModal.vue'
   import { useUserStore } from '/@/store/modules/user';
-  const queryParam = reactive<any>({});
-  const checkedKeys = ref<Array<string | number>>([]);
-  const userStore = useUserStore();
-  //注册model
-  const [registerModal, {openModal}] = useModal();
-  //注册table数据
-  const { prefixCls,tableContext,onExportXls,onImportXls } = useListPage({
-      tableProps:{
-           title: '系统产品包信息',
-           api: list,
-           columns,
-           canResize:false,
-           formConfig: {
-              //labelWidth: 120,
-              schemas: searchFormSchema,
-              autoSubmitOnEnter:true,
-              showAdvancedButton:true,
-              fieldMapToNumber: [
-              ],
-              fieldMapToTime: [
-              ],
-            },
-           actionColumn: {
-               width: 120,
-               fixed:'right'
-            },
-            beforeFetch: (params) => {
-              return Object.assign(params, queryParam);
-            },
-      },
-       exportConfig: {
-            name:"系统产品包信息",
-            url: getExportUrl,
-            params: queryParam,
-          },
-          importConfig: {
-            url: getImportUrl,
-            success: handleSuccess
-          },
-  })
 
-  const [registerTable, {reload},{ rowSelection, selectedRowKeys }] = tableContext
+  const formRef = ref();
+  const queryParam = reactive<any>({});
+  const toggleSearchStatus = ref<boolean>(false);
+  const registerModal = ref();
+  const userStore = useUserStore();
+  //注册table数据
+  const { prefixCls, tableContext, onExportXls, onImportXls } = useListPage({
+    tableProps: {
+      title: '系统产品包信息',
+      api: list,
+      columns,
+      canResize:false,
+      useSearchForm: false,
+      actionColumn: {
+        width: 120,
+        fixed: 'right',
+      },
+      beforeFetch: async (params) => {
+        return Object.assign(params, queryParam);
+      },
+    },
+    exportConfig: {
+      name: "系统产品包信息",
+      url: getExportUrl,
+      params: queryParam,
+    },
+	  importConfig: {
+	    url: getImportUrl,
+	    success: handleSuccess
+	  },
+  });
+  const [registerTable, { reload, collapseAll, updateTableDataRecord, findTableDataRecord, getDataSource }, { rowSelection, selectedRowKeys }] = tableContext;
+  const labelCol = reactive({
+    xs:24,
+    sm:4,
+    xl:6,
+    xxl:4
+  });
+  const wrapperCol = reactive({
+    xs: 24,
+    sm: 20,
+  });
 
   // 高级查询配置
   const superQueryConfig = reactive(superQuerySchema);
@@ -99,92 +197,131 @@
     Object.keys(params).map((k) => {
       queryParam[k] = params[k];
     });
-    reload();
+    searchQuery();
   }
-   /**
-    * 新增事件
-    */
+
+  /**
+   * 新增事件
+   */
   function handleAdd() {
-     openModal(true, {
-       isUpdate: false,
-       showFooter: true,
-     });
+    registerModal.value.disableSubmit = false;
+    registerModal.value.add();
   }
-   /**
-    * 编辑事件
-    */
+  
+  /**
+   * 编辑事件
+   */
   function handleEdit(record: Recordable) {
-     openModal(true, {
-       record,
-       isUpdate: true,
-       showFooter: true,
-     });
-   }
-   /**
-    * 详情
+    registerModal.value.disableSubmit = false;
+    registerModal.value.edit(record);
+  }
+   
+  /**
+   * 详情
    */
   function handleDetail(record: Recordable) {
-     openModal(true, {
-       record,
-       isUpdate: true,
-       showFooter: false,
-     });
-   }
-   /**
-    * 删除事件
-    */
+    registerModal.value.disableSubmit = true;
+    registerModal.value.edit(record);
+  }
+   
+  /**
+   * 删除事件
+   */
   async function handleDelete(record) {
-     await deleteOne({id: record.id}, handleSuccess);
-   }
-   /**
-    * 批量删除事件
-    */
+    await deleteOne({ id: record.id }, handleSuccess);
+  }
+   
+  /**
+   * 批量删除事件
+   */
   async function batchHandleDelete() {
-     await batchDelete({ids: selectedRowKeys.value}, handleSuccess);
-   }
-   /**
-    * 成功回调
-    */
+    await batchDelete({ ids: selectedRowKeys.value }, handleSuccess);
+  }
+   
+  /**
+   * 成功回调
+   */
   function handleSuccess() {
-      (selectedRowKeys.value = []) && reload();
-   }
-   /**
-      * 操作栏
-      */
-  function getTableAction(record){
-       return [
-         {
-           label: '编辑',
-           onClick: handleEdit.bind(null, record),
-           auth: 'syspack:sys_pack:edit'
-         }
-       ]
-   }
-     /**
-        * 下拉操作栏
-        */
-  function getDropDownAction(record){
-       return [
-         {
-           label: '详情',
-           onClick: handleDetail.bind(null, record),
-         }, {
-           label: '删除',
-           popConfirm: {
-             title: '是否确认删除',
-             confirm: handleDelete.bind(null, record),
-             placement: 'topLeft',
-           },
-           auth: 'syspack:sys_pack:delete'
-         }
-       ]
-   }
+    (selectedRowKeys.value = []) && reload();
+  }
+   
+  /**
+   * 操作栏
+   */
+  function getTableAction(record) {
+    return [
+      {
+        label: '编辑',
+        onClick: handleEdit.bind(null, record),
+        auth: 'syspack:sys_pack:edit'
+      },
+    ];
+  }
+   
+  /**
+   * 下拉操作栏
+   */
+  function getDropDownAction(record) {
+    return [
+      {
+        label: '详情',
+        onClick: handleDetail.bind(null, record),
+      }, {
+        label: '删除',
+        popConfirm: {
+          title: '是否确认删除',
+          confirm: handleDelete.bind(null, record),
+          placement: 'topLeft',
+        },
+        auth: 'syspack:sys_pack:delete'
+      }
+    ]
+  }
+
+  /**
+   * 查询
+   */
+  function searchQuery() {
+    reload();
+  }
+  
+  /**
+   * 重置
+   */
+  function searchReset() {
+    formRef.value.resetFields();
+    selectedRowKeys.value = [];
+    //刷新数据
+    reload();
+  }
+  
+
 
 
 </script>
 
 <style lang="less" scoped>
-  :deep(.ant-picker),:deep(.ant-input-number){
-    width: 100%;
+  .jeecg-basic-table-form-container {
+    padding: 0;
+    .table-page-search-submitButtons {
+      display: block;
+      margin-bottom: 24px;
+      white-space: nowrap;
+    }
+    .query-group-cust{
+      min-width: 100px !important;
+    }
+    .query-group-split-cust{
+      width: 30px;
+      display: inline-block;
+      text-align: center
+    }
+    .ant-form-item:not(.ant-form-item-with-help){
+      margin-bottom: 16px;
+      height: 32px;
+    }
+    :deep(.ant-picker),:deep(.ant-input-number){
+      width: 100%;
+    }
   }
 </style>
--- src/views/syspack/components/SysPackForm.vue
+++ src/views/syspack/components/SysPackForm.vue
@@ -1,70 +1,220 @@
 <template>
-    <div style="min-height: 400px">
-        <BasicForm @register="registerForm"></BasicForm>
-        <div style="width: 100%;text-align: center" v-if="!formDisabled">
-            <a-button @click="submitForm" pre-icon="ant-design:check" type="primary">提 交</a-button>
-        </div>
-    </div>
+  <a-spin :spinning="confirmLoading">
+    <JFormContainer :disabled="disabled">
+      <template #detail>
+        <a-form ref="formRef" class="antd-modal-form" :labelCol="labelCol" :wrapperCol="wrapperCol" name="SysPackForm">
+          <a-row>
+						<a-col :span="12">
+							<a-form-item label="产品名称" v-bind="validateInfos.packName" id="SysPackForm-packName" name="packName">
+								<a-input v-model:value="formData.packName" placeholder="请输入产品名称"  allow-clear ></a-input>
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="产品类别(单机版/云端版)" v-bind="validateInfos.category" id="SysPackForm-category" name="category">
+								<j-dict-select-tag v-model:value="formData.category" dictCode="" placeholder="请选择产品类别(单机版/云端版)"  allow-clear />
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="产品类型(送货单版/进销存版)" v-bind="validateInfos.type" id="SysPackForm-type" name="type">
+								<j-dict-select-tag v-model:value="formData.type" dictCode="" placeholder="请选择产品类型(送货单版/进销存版)"  allow-clear />
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="支持企业数(单机1个公司、云端版支持4家公司切换开单)" v-bind="validateInfos.orgNum" id="SysPackForm-orgNum" name="orgNum">
+								<a-input-number v-model:value="formData.orgNum" placeholder="请输入支持企业数(单机1个公司、云端版支持4家公司切换开单)" style="width: 100%" />
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="支持账号数(云端版支持添加2个子账号，授权后最大支持添加12个子账号)" v-bind="validateInfos.accountNum" id="SysPackForm-accountNum" name="accountNum">
+								<a-input-number v-model:value="formData.accountNum" placeholder="请输入支持账号数(云端版支持添加2个子账号，授权后最大支持添加12个子账号)" style="width: 100%" />
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="支持商品数量()" v-bind="validateInfos.goodsNum" id="SysPackForm-goodsNum" name="goodsNum">
+								<a-input-number v-model:value="formData.goodsNum" placeholder="请输入支持商品数量()" style="width: 100%" />
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="产品标准价格" v-bind="validateInfos.price" id="SysPackForm-price" name="price">
+								<a-input-number v-model:value="formData.price" placeholder="请输入产品标准价格" style="width: 100%" />
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="产品折扣" v-bind="validateInfos.discounted" id="SysPackForm-discounted" name="discounted">
+								<a-input-number v-model:value="formData.discounted" placeholder="请输入产品折扣" style="width: 100%" />
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="产品折扣价" v-bind="validateInfos.discountedPrice" id="SysPackForm-discountedPrice" name="discountedPrice">
+								<a-input-number v-model:value="formData.discountedPrice" placeholder="请输入产品折扣价" style="width: 100%" />
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="规格" v-bind="validateInfos.specification" id="SysPackForm-specification" name="specification">
+								<a-input-number v-model:value="formData.specification" placeholder="请输入规格" style="width: 100%" />
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="规格单位" v-bind="validateInfos.specificationUnit" id="SysPackForm-specificationUnit" name="specificationUnit">
+								<a-input v-model:value="formData.specificationUnit" placeholder="请输入规格单位"  allow-clear ></a-input>
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="产品描述" v-bind="validateInfos.discription" id="SysPackForm-discription" name="discription">
+								<a-textarea v-model:value="formData.discription" :rows="4" placeholder="请输入产品描述" />
+							</a-form-item>
+						</a-col>
+						<a-col :span="12">
+							<a-form-item label="备注说明" v-bind="validateInfos.remarks" id="SysPackForm-remarks" name="remarks">
+								<a-textarea v-model:value="formData.remarks" :rows="4" placeholder="请输入备注说明" />
+							</a-form-item>
+						</a-col>
+          </a-row>
+        </a-form>
+      </template>
+    </JFormContainer>
+  </a-spin>
 </template>
 
-<script lang="ts">
-    import {BasicForm, useForm} from '/@/components/Form/index';
-    import {computed, defineComponent} from 'vue';
-    import {defHttp} from '/@/utils/http/axios';
-    import { propTypes } from '/@/utils/propTypes';
-    import {getBpmFormSchema} from '../SysPack.data';
-    import {saveOrUpdate} from '../SysPack.api';
-    
-    export default defineComponent({
-        name: "SysPackForm",
-        components:{
-            BasicForm
-        },
-        props:{
-            formData: propTypes.object.def({}),
-            formBpm: propTypes.bool.def(true),
-        },
-        setup(props){
-            const [registerForm, { setFieldsValue, setProps, getFieldsValue }] = useForm({
-                labelWidth: 150,
-                schemas: getBpmFormSchema(props.formData),
-                showActionButtonGroup: false,
-                baseColProps: {span: 12}
-            });
+<script lang="ts" setup>
+  import { ref, reactive, defineExpose, nextTick, defineProps, computed, onMounted } from 'vue';
+  import { defHttp } from '/@/utils/http/axios';
+  import { useMessage } from '/@/hooks/web/useMessage';
+  import JDictSelectTag from '/@/components/Form/src/jeecg/components/JDictSelectTag.vue';
+  import { getValueType } from '/@/utils';
+  import { saveOrUpdate } from '../SysPack.api';
+  import { Form } from 'ant-design-vue';
+  import JFormContainer from '/@/components/Form/src/container/JFormContainer.vue';
+  const props = defineProps({
+    formDisabled: { type: Boolean, default: false },
+    formData: { type: Object, default: () => ({})},
+    formBpm: { type: Boolean, default: true }
+  });
+  const formRef = ref();
+  const useForm = Form.useForm;
+  const emit = defineEmits(['register', 'ok']);
+  const formData = reactive<Record<string, any>>({
+    id: '',
+    packName: '',   
+    category: '',   
+    type: '',   
+    orgNum: undefined,
+    accountNum: undefined,
+    goodsNum: undefined,
+    price: undefined,
+    discounted: undefined,
+    discountedPrice: undefined,
+    specification: undefined,
+    specificationUnit: '',   
+    discription: '',   
+    remarks: '',   
+  });
+  const { createMessage } = useMessage();
+  const labelCol = ref<any>({ xs: { span: 24 }, sm: { span: 5 } });
+  const wrapperCol = ref<any>({ xs: { span: 24 }, sm: { span: 16 } });
+  const confirmLoading = ref<boolean>(false);
+  //表单验证
+  const validatorRules = reactive({
+    packName: [{ required: true, message: '请输入产品名称!'},],
+    discription: [{ required: true, message: '请输入产品描述!'},],
+  });
+  const { resetFields, validate, validateInfos } = useForm(formData, validatorRules, { immediate: false });
 
-            const formDisabled = computed(()=>{
-                if(props.formData.disabled === false){
-                    return false;
-                }
-                return true;
-            });
+  // 表单禁用
+  const disabled = computed(()=>{
+    if(props.formBpm === true){
+      if(props.formData.disabled === false){
+        return false;
+      }else{
+        return true;
+      }
+    }
+    return props.formDisabled;
+  });
 
-            let formData = {};
-            const queryByIdUrl = '/syspack/sysPack/queryById';
-            async function initFormData(){
-                let params = {id: props.formData.dataId};
-                const data = await defHttp.get({url: queryByIdUrl, params});
-                formData = {...data}
-                //设置表单的值
-                await setFieldsValue(formData);
-                //默认是禁用
-                await setProps({disabled: formDisabled.value})
-            }
+  
+  /**
+   * 新增
+   */
+  function add() {
+    edit({});
+  }
 
-            async function submitForm() {
-                let data = getFieldsValue();
-                let params = Object.assign({}, formData, data);
-                console.log('表单数据', params)
-                await saveOrUpdate(params, true)
-            }
-
-            initFormData();
-            
-            return {
-                registerForm,
-                formDisabled,
-                submitForm,
-            }
+  /**
+   * 编辑
+   */
+  function edit(record) {
+    nextTick(() => {
+      resetFields();
+      const tmpData = {};
+      Object.keys(formData).forEach((key) => {
+        if(record.hasOwnProperty(key)){
+          tmpData[key] = record[key]
         }
+      })
+      //赋值
+      Object.assign(formData, tmpData);
     });
+  }
+
+  /**
+   * 提交数据
+   */
+  async function submitForm() {
+    try {
+      // 触发表单验证
+      await validate();
+    } catch ({ errorFields }) {
+      if (errorFields) {
+        const firstField = errorFields[0];
+        if (firstField) {
+          formRef.value.scrollToField(firstField.name, { behavior: 'smooth', block: 'center' });
+        }
+      }
+      return Promise.reject(errorFields);
+    }
+    confirmLoading.value = true;
+    const isUpdate = ref<boolean>(false);
+    //时间格式化
+    let model = formData;
+    if (model.id) {
+      isUpdate.value = true;
+    }
+    //循环数据
+    for (let data in model) {
+      //如果该数据是数组并且是字符串类型
+      if (model[data] instanceof Array) {
+        let valueType = getValueType(formRef.value.getProps, data);
+        //如果是字符串类型的需要变成以逗号分割的字符串
+        if (valueType === 'string') {
+          model[data] = model[data].join(',');
+        }
+      }
+    }
+    await saveOrUpdate(model, isUpdate.value)
+      .then((res) => {
+        if (res.success) {
+          createMessage.success(res.message);
+          emit('ok');
+        } else {
+          createMessage.warning(res.message);
+        }
+      })
+      .finally(() => {
+        confirmLoading.value = false;
+      });
+  }
+
+
+  defineExpose({
+    add,
+    edit,
+    submitForm,
+  });
 </script>
+
+<style lang="less" scoped>
+  .antd-modal-form {
+    padding: 14px;
+  }
+</style>
