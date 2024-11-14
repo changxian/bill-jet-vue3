5e613121bc0e6fbe323f9207455bf4d72ac4a5c5
ylzhou <1216202141@qq.com>
2024-11-14 22:57:59 +0800

    AutoComplete

--- src/views/statistics/statistics/Statistics.vue
+++ src/views/statistics/statistics/Statistics.vue
@@ -1,24 +1,42 @@
 <template>
-    <div class="statistics-page">
-<!--      <TestInput/>-->
-      <ModuleTotal/>
-      <ModuleDateTotal/>
-      <HotGoods/>
-    </div> 
+  <div class="statistics-page">
+    <!--      <form>-->
+    <!--        <input v-model:value="formData.name">&ndash;&gt;-->
+
+    <!--        <TestInput v-model="val"  />-->
+    <!--        <TestInput v-model="formData.remark2"   />-->
+    <!--        <TestInput v-model="formData.remark3"   />-->
+    <!--      </form>-->
+    <!--      <button @click="aaa">button</button>-->
+    <ModuleTotal/>
+    <ModuleDateTotal/>
+    <HotGoods/>
+  </div>
 </template>
 
 <script lang="ts" setup>
-//
-//import TestInput from './TestInput.vue'
+
 import ModuleTotal from './ModuleTotal.vue'
 import ModuleDateTotal from './ModuleDateTotal.vue'
 import HotGoods from './HotGoods.vue'
- 
- 
+import {reactive, ref} from "vue";
+
+const formData = reactive({
+  remark: 'aaa',
+  remark2: '22',
+  remark3: 'aa33a',
+  name: "22",
+})
+const val = ref('aa')
+
+function aaa() {
+  console.log("====aaa======", formData);
+}
+
 </script>
 <style lang="less" scoped>
-.statistics-page{
-  margin-left:10px;
+.statistics-page {
+  margin-left: 10px;
 }
- 
+
 </style>
--- src/views/statistics/statistics/TestInput.vue
+++ src/views/statistics/statistics/TestInput.vue
@@ -1,43 +1,80 @@
 <template>
 
-    <div class="main">
-            <AutoComplete
-             style="width:300px"
-             v-model:value="queryValue"
-            :options="options"
-            @search="onSearch"
-            placeholder="请输入关键字"
-            @select="onSelect"/>
-         <a-button @click="getVal">获取输入值</a-button>
-    </div>
-     
+  <div class="main">
+    <AutoComplete
+      style="width:300px"
+      :value="modelValue"
+      :options="options"
+      @search="onSearch"
+      placeholder="请输入关键字"
+      @select="onSelect"/>
+    <a-button @click="getVal">获取输入值</a-button>
+  </div>
+
 </template>
 <script lang="ts" setup>
-import  { AutoComplete } from 'ant-design-vue';
- import { ref} from 'vue';
+import {AutoComplete} from 'ant-design-vue';
+import {defineEmits, defineProps, ref} from 'vue';
+import {string} from "vue-types";
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  }
+});
+const emits = defineEmits(['update:modelValue']);
 
 const queryValue = ref('')
 const options = ref<any>([])
-     function onSearch(value) {
-        console.log('查询参数value: ', value )
-        if(value.length < 4){
-            options.value = !value ? [] : [value, value + '1', value + '2'].map(val => ({
-                value: val,
-                text: val,
-            }));
-        }else {
-            options.value = []
-        }
+function debounce (fn, delay = 1000) {
+  let time = null
+  return function () {
+    // 获取当前this
+    let that = this
+    // 判断是否已经存在，如果存在直接清除
+    if (time) {
+      clearTimeout(time)
     }
-    function onSelect(value) {
-      console.log('选中的值:', value);
-      console.log('queryValue', queryValue.value)
-    }
-    function getVal(){
-         console.log('queryValue', queryValue.value)
-    }
+    time = setTimeout(() => {
+      // 使fn 中this，执行当前调用者，并传入参数
+      fn.apply(that, arguments)
+    }, delay)
+  }
+}
+let nowTime = new Date().getTime()
+let lastTime = new Date().getTime()
+function queryFn(){
+  console.log('queryFn======',props.modelValue)
+  const value = props.modelValue
+  lastTime = new Date().getTime()
+  // debounce(queryFn,2000)()
+
+    options.value.push({
+      value: value+"bbb",
+      text: value+"bbb",
+    })
+
+}
+function onSearch(value) {
+  console.log('查询参数value: ', value)
+  emits('update:modelValue', value)
+  nowTime = new Date().getTime()
+  if(nowTime - lastTime > 2 * 1000 ){
+    queryFn()
+  }
+
+}
+
+function onSelect(value) {
+  emits('update:modelValue', value)
+}
+
+function getVal() {
+  console.log('queryValue', queryValue.value)
+}
 </script>
 <style lang="less" scoped>
 
- 
+
 </style>
