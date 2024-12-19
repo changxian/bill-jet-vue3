62f72b84515499e3f340d2b3b8cd1e29c55c285e
ylzhou <1216202141@qq.com>
2024-12-19 21:55:04 +0800

    result

--- src/utils/http/axios/index.ts
+++ src/utils/http/axios/index.ts
@@ -1,27 +1,28 @@
 // axios配置  可自行根据项目进行更改，只需更改该文件即可，其他文件可以不动
 // The axios configuration can be changed according to the project, just change the file, other files can be left unchanged
 
-import type { AxiosResponse } from 'axios';
-import type { RequestOptions, Result } from '/#/axios';
-import type { AxiosTransform, CreateAxiosOptions } from './axiosTransform';
-import { VAxios } from './Axios';
-import { checkStatus } from './checkStatus';
-import { router } from '/@/router';
-import { useGlobSetting } from '/@/hooks/setting';
-import { useMessage } from '/@/hooks/web/useMessage';
-import { RequestEnum, ResultEnum, ContentTypeEnum, ConfigEnum } from '/@/enums/httpEnum';
-import { isString } from '/@/utils/is';
-import { getToken, getTenantId } from '/@/utils/auth';
-import { setObjToUrlParams, deepMerge } from '/@/utils';
+import type {AxiosResponse} from 'axios';
+import type {RequestOptions, Result} from '/#/axios';
+import type {AxiosTransform, CreateAxiosOptions} from './axiosTransform';
+import {VAxios} from './Axios';
+import {checkStatus} from './checkStatus';
+import {router} from '/@/router';
+import {useGlobSetting} from '/@/hooks/setting';
+import {useMessage} from '/@/hooks/web/useMessage';
+import {RequestEnum, ResultEnum, ContentTypeEnum, ConfigEnum} from '/@/enums/httpEnum';
+import {isString} from '/@/utils/is';
+import {getToken, getTenantId} from '/@/utils/auth';
+import {setObjToUrlParams, deepMerge} from '/@/utils';
 import signMd5Utils from '/@/utils/encryption/signMd5Utils';
-import { useErrorLogStoreWithOut } from '/@/store/modules/errorLog';
-import { useI18n } from '/@/hooks/web/useI18n';
-import { joinTimestamp, formatRequestDate } from './helper';
-import { useUserStoreWithOut } from '/@/store/modules/user';
-import { cloneDeep } from "lodash-es";
+import {useErrorLogStoreWithOut} from '/@/store/modules/errorLog';
+import {useI18n} from '/@/hooks/web/useI18n';
+import {joinTimestamp, formatRequestDate} from './helper';
+import {useUserStoreWithOut} from '/@/store/modules/user';
+import {cloneDeep} from "lodash-es";
+
 const globSetting = useGlobSetting();
 const urlPrefix = globSetting.urlPrefix;
-const { createMessage, createErrorModal } = useMessage();
+const {createMessage, createErrorModal} = useMessage();
 
 /**
  * @description: 数据处理，方便区分多种处理方式
@@ -31,8 +32,8 @@ const transform: AxiosTransform = {
    * @description: 处理请求数据。如果数据不是预期格式，可直接抛出错误
    */
   transformRequestHook: (res: AxiosResponse<Result>, options: RequestOptions) => {
-    const { t } = useI18n();
-    const { isTransformResponse, isReturnNativeResponse } = options;
+    const {t} = useI18n();
+    const {isTransformResponse, isReturnNativeResponse} = options;
     // 是否返回原生响应头 比如：需要获取响应头时使用该属性
     if (isReturnNativeResponse) {
       return res;
@@ -44,13 +45,13 @@ const transform: AxiosTransform = {
     }
     // 错误的时候返回
 
-    const { data } = res;
+    const {data} = res;
     if (!data) {
       // return '[HTTP] Request has no return value';
       throw new Error(t('sys.api.apiRequestFailed'));
     }
     //  这里 code，result，message为 后台统一的字段，需要在 types.ts内修改为项目自己的接口返回格式
-    const { code, result, message, success,extraInfo } = data;
+    const {code, result, message, success, extraInfo} = data;
     // 这里逻辑可以根据项目进行修改
     const hasSuccess = data && Reflect.has(data, 'code') && (code === ResultEnum.SUCCESS || code === 200);
     if (hasSuccess) {
@@ -58,7 +59,11 @@ const transform: AxiosTransform = {
         //信息成功提示
         createMessage.success(message);
       }
-      return   result instanceof Object?{...result,extraInfo}:result;
+      if (extraInfo && (result instanceof Object)) {
+        return {...result, extraInfo}
+      } else {
+        return result
+      }
     }
 
     // 在此处根据自己项目的实际情况对不同的code执行不同的操作
@@ -80,7 +85,7 @@ const transform: AxiosTransform = {
     // errorMessageMode=‘modal’的时候会显示modal错误弹窗，而不是消息提示，用于一些比较重要的错误
     // errorMessageMode='none' 一般是调用时明确表示不希望自动弹出错误提示
     if (options.errorMessageMode === 'modal') {
-      createErrorModal({ title: t('sys.api.errorTip'), content: timeoutMsg });
+      createErrorModal({title: t('sys.api.errorTip'), content: timeoutMsg});
     } else if (options.errorMessageMode === 'message') {
       createMessage.error(timeoutMsg);
     }
@@ -90,13 +95,13 @@ const transform: AxiosTransform = {
 
   // 请求之前处理config
   beforeRequestHook: (config, options) => {
-    const { apiUrl, joinPrefix, joinParamsToUrl, formatDate, joinTime = true, urlPrefix } = options;
+    const {apiUrl, joinPrefix, joinParamsToUrl, formatDate, joinTime = true, urlPrefix} = options;
 
     //update-begin---author:scott ---date:2024-02-20  for：以http开头的请求url，不拼加前缀--
     // http开头的请求url，不加前缀
     let isStartWithHttp = false;
     const requestUrl = config.url;
-    if(requestUrl!=null && (requestUrl.startsWith("http:") || requestUrl.startsWith("https:"))){
+    if (requestUrl != null && (requestUrl.startsWith("http:") || requestUrl.startsWith("https:"))) {
       isStartWithHttp = true;
     }
     if (!isStartWithHttp && joinPrefix) {
@@ -107,7 +112,7 @@ const transform: AxiosTransform = {
       config.url = `${apiUrl}${config.url}`;
     }
     //update-end---author:scott ---date::2024-02-20  for：以http开头的请求url，不拼加前缀--
-    
+
     const params = config.params || {};
     const data = config.data || false;
     formatDate && data && !isString(data) && formatRequestDate(data);
@@ -150,7 +155,7 @@ const transform: AxiosTransform = {
     // 请求之前处理config
     const token = getToken();
     let tenantId: string | number = getTenantId();
-    
+
     //update-begin---author:wangshuai---date:2024-04-16---for:【QQYUN-9005】发送短信加签。解决没有token无法加签---
     // 将签名和时间戳，添加在请求接口 Header
     config.headers[ConfigEnum.TIMESTAMP] = signMd5Utils.getTimestamp();
@@ -167,7 +172,7 @@ const transform: AxiosTransform = {
       // jwt token
       config.headers.Authorization = options.authenticationScheme ? `${options.authenticationScheme} ${token}` : token;
       config.headers[ConfigEnum.TOKEN] = token;
-      
+
       // 将签名和时间戳，添加在请求接口 Header
       //config.headers[ConfigEnum.TIMESTAMP] = signMd5Utils.getTimestamp();
       //config.headers[ConfigEnum.Sign] = signMd5Utils.getSign(config.url, config.params);
@@ -194,7 +199,7 @@ const transform: AxiosTransform = {
         config.headers[ConfigEnum.X_LOW_APP_ID] = routeParams.appId;
         // lowApp自定义筛选条件
         if (routeParams.lowAppFilter) {
-          config.params = { ...config.params, ...JSON.parse(routeParams.lowAppFilter as string) };
+          config.params = {...config.params, ...JSON.parse(routeParams.lowAppFilter as string)};
           delete routeParams.lowAppFilter;
         }
       }
@@ -216,10 +221,10 @@ const transform: AxiosTransform = {
    * @description: 响应错误处理
    */
   responseInterceptorsCatch: (error: any) => {
-    const { t } = useI18n();
+    const {t} = useI18n();
     const errorLogStore = useErrorLogStoreWithOut();
     errorLogStore.addAjaxErrorInfo(error);
-    const { response, code, message, config } = error || {};
+    const {response, code, message, config} = error || {};
     const errorMessageMode = config?.requestOptions?.errorMessageMode || 'none';
     //scott 20211022 token失效提示信息
     //const msg: string = response?.data?.error?.message ?? '';
@@ -237,7 +242,7 @@ const transform: AxiosTransform = {
 
       if (errMessage) {
         if (errorMessageMode === 'modal') {
-          createErrorModal({ title: t('sys.api.errorTip'), content: errMessage });
+          createErrorModal({title: t('sys.api.errorTip'), content: errMessage});
         } else if (errorMessageMode === 'message') {
           createMessage.error(errMessage);
         }
@@ -264,7 +269,7 @@ function createAxios(opt?: Partial<CreateAxiosOptions>) {
         timeout: 10 * 1000,
         // 基础接口地址
         // baseURL: globSetting.apiUrl,
-        headers: { 'Content-Type': ContentTypeEnum.JSON },
+        headers: {'Content-Type': ContentTypeEnum.JSON},
         // 如果是form-data格式
         // headers: { 'Content-Type': ContentTypeEnum.FORM_URLENCODED },
         // 数据处理方式
@@ -301,6 +306,7 @@ function createAxios(opt?: Partial<CreateAxiosOptions>) {
     )
   );
 }
+
 export const defHttp = createAxios();
 
 // other api url
