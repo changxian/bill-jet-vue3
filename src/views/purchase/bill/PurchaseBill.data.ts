775dbe4789d30db6be14370861719a06dec1c645
乐爷 <672882238@qq.com>
2025-04-10 00:09:18 +0800

    1. 进货单页面优化；

--- src/views/purchase/bill/PurchaseBill.data.ts
+++ src/views/purchase/bill/PurchaseBill.data.ts
@@ -3,11 +3,16 @@ import { useUserStore } from '@/store/modules/user';
 import { ref } from 'vue';
 const userStore = useUserStore();
 const billSetting = userStore.getBillSetting;
+const countColTitle = ref('');
 const weightColTitle = ref('');
 const areaColTitle = ref('');
 const volumeColTitle = ref('');
 if (billSetting.dynaFieldsGroup['1']) {
   billSetting.dynaFieldsGroup['1'].forEach((item) => {
+    // 数量小计
+    if (item.fieldName === 'count') {
+      countColTitle.value = item.fieldTitle || '';
+    }
     // 重量小计
     if (item.fieldName === 'weightSubtotal') {
       weightColTitle.value = item.fieldTitle || '';
@@ -47,86 +52,118 @@ export const columns: BasicColumn[] = [
     title: '单号',
     align: 'center',
     dataIndex: 'billNo',
+    width: 150,
+    resizable: true,
   },
   {
     title: '开单类型',
     align: 'center',
     dataIndex: 'type_dictText',
+    width: 100,
+    resizable: true,
     slots: { customRender: 'type_dictText' },
   },
   {
     title: '开单日期',
     align: 'center',
     dataIndex: 'billDate',
+    width: 150,
+    resizable: true,
   },
   {
     title: '公司名称',
     align: 'center',
     dataIndex: 'companyName',
+    width: 150,
+    resizable: true,
   },
   {
     title: '供应商名称',
     align: 'center',
     dataIndex: 'supplierName',
+    width: 150,
+    resizable: true,
   },
   {
     title: '供应商电话',
     align: 'center',
     dataIndex: 'supplierPhone',
+    width: 120,
+    resizable: true,
   },
   {
     title: '供应商联系人',
     align: 'center',
     dataIndex: 'supplierContact',
+    width: 100,
+    resizable: true,
   },
   {
     title: '供应商地址',
     align: 'center',
     dataIndex: 'supplierAddress',
+    width: 150,
+    resizable: true,
   },
   {
-    title: '数量',
+    title: '数量合计' + (countColTitle.value ? '(' + countColTitle.value + ')' : ''),
     align: 'center',
     dataIndex: 'count',
+    width: 100,
+    resizable: true,
   },
   {
     title: '本单金额',
     align: 'center',
     dataIndex: 'amount',
+    width: 100,
+    resizable: true,
   },
   {
     title: '已付款金额',
     align: 'center',
     dataIndex: 'paymentAmount',
+    width: 100,
+    resizable: true,
   },
   {
     title: '优惠金额',
     align: 'center',
     dataIndex: 'discountAmount',
+    width: 100,
+    resizable: true,
   },
   {
     title: '欠款金额',
     align: 'center',
     dataIndex: 'debtAmount',
+    width: 100,
+    resizable: true,
   },
   {
-    title: '重量合计(' + weightColTitle.value + ')',
+    title: '重量合计' + (weightColTitle.value ? '(' + weightColTitle.value + ')' : ''),
     align: 'center',
     dataIndex: 'weight',
+    width: 100,
+    resizable: true,
     defaultHidden:!billSetting.showWeightCol,
     ifShow: billSetting.showWeightCol || false,
   },
   {
-    title: '面积合计(' + areaColTitle.value + ')',
+    title: '面积合计' + (areaColTitle.value ? '(' + areaColTitle.value + ')' : ''),
     align: 'center',
     dataIndex: 'area',
+    width: 100,
+    resizable: true,
     defaultHidden:!billSetting.showAreaCol,
     ifShow: billSetting.showAreaCol || false,
   },
   {
-    title: '体积合计(' + volumeColTitle.value + ')',
+    title: '体积合计' + (volumeColTitle.value ? '(' + volumeColTitle.value + ')' : ''),
     align: 'center',
     dataIndex: 'volume',
+    width: 100,
+    resizable: true,
     defaultHidden:!billSetting.showVolumeCol,
     ifShow: billSetting.showVolumeCol || false,
   },
@@ -134,42 +171,58 @@ export const columns: BasicColumn[] = [
     title: '往期欠款金额',
     align: 'center',
     dataIndex: 'hisDebtAmount',
+    width: 100,
+    resizable: true,
   },
   {
     title: '送货车号',
     align: 'center',
     dataIndex: 'careNo',
+    width: 100,
+    resizable: true,
   },
   {
     title: '合同号',
     align: 'center',
     dataIndex: 'contractCode',
+    width: 100,
+    resizable: true,
   },
   {
     title: '状态',
     align: 'center',
     dataIndex: 'status_dictText',
     slots: { customRender: 'status_dictText' },
+    width: 100,
+    resizable: true,
   },
   {
     title: '开票状态',
     align: 'center',
     dataIndex: 'invoiceStatus_dictText',
+    width: 100,
+    resizable: true,
   },
   {
     title: '业务员',
     align: 'center',
     dataIndex: 'userName',
+    width: 100,
+    resizable: true,
   },
   {
     title: '备注',
     align: 'center',
     dataIndex: 'remark',
+    width: 120,
+    resizable: true,
   },
   {
     title: '制单人',
     align: 'center',
     dataIndex: 'operatorName',
+    width: 100,
+    resizable: true,
   },
 ];
 
@@ -244,6 +297,8 @@ export const detailColumns: BasicColumn[] = [
     title: '宽',
     align: 'center',
     dataIndex: 'width',
+    width: 60,
+    resizable: true,
     ifShow: billSetting.showLengthWidthCol || billSetting.showLengthWidthHeightCol || false,
   },
   {
