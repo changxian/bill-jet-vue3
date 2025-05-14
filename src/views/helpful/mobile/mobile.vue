3d627a1c69c0b9504a465baf57c951a1c66281a9
乐爷 <672882238@qq.com>
2025-05-14 22:42:34 +0800

    1. 替换小程序二维码；

--- src/views/helpful/mobile/mobile.vue
+++ src/views/helpful/mobile/mobile.vue
@@ -2,14 +2,14 @@
   <PageWrapper title="手机端">
     <template #headerContent>
       <div class="flex justify-between items-center">
-        <div class="ant-result-title">手机端小程序请在微信里搜索“XXXXXXX”开始应用，或者打开微信扫描二维码开始使用。</div>
+        <div class="ant-result-title">手机端小程序请在微信里搜索“鑫泓软件”开始应用，或者打开微信扫描二维码开始使用。</div>
       </div>
       <div class="flex items-center ant-result" style="padding: 48px 32px">
-        <div class="ant-result-icon">
+        <!--<div class="ant-result-icon">
           <img src="/src/assets/images/mobile/mobile.png" />
-        </div>
+        </div>-->
         <div class="ant-result-icon" style="margin-left: 80px">
-          <img src="/src/assets/images/mobile/qrcode.png" />
+          <img style="height: 400px" src="/src/assets/images/mobile/qrcode.jpg" />
         </div>
       </div>
     </template>
